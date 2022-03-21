2b38f64bc95e4a12ceae7ce4640fe3918246a671
scopp <osc.car2@gmail.com>
2022-03-21 21:36:48 +0100

    Added reload command

--- src/back/plugin/plugin-internal.js
+++ src/back/plugin/plugin-internal.js
@@ -1,6 +1,11 @@
 const Plugin = require('./plugin');
 const { app } = require("electron");
 
+const LITERALS = {
+	'_EXIT_': 'Exits ualth',
+	'_RELOAD_': 'Reloads ualth configuration',
+};
+
 class SearchEnginePlugin extends Plugin {
 	keyName = 'internalCommands'
 	type = 'INT';
@@ -9,13 +14,17 @@ class SearchEnginePlugin extends Plugin {
 		this.generateCommandDefinitions((data, key) => ({
 			command: data,
 			id: `${this.type}_${data}`,
-			title: key
+			title: LITERALS[data] ?? key
 		}));
 	}
 
 	perform(entry) {
 		if (entry.command === '_EXIT_') {
 			app.quit();
+
+		} else if (entry.command === '_RELOAD_') {
+			app.relaunch();
+			app.exit();
 		}
 	}
 }
