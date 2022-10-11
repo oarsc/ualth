4be3eb2c985042100927e7ddcaf43551aa97e6b8
scopp <osc.car2@gmail.com>
2022-10-11 08:24:35 +0200

    Read all profiles from firefox folder

--- src/back/command/command-firefox.js
+++ src/back/command/command-firefox.js
@@ -3,10 +3,15 @@ const homedir = require('os').homedir();
 const jsonlz4 = require('jsonlz4-decompress');
 const fs = require('fs');
 const { shell } = require('electron');
+const { spawn } = require("child_process");
+
+const WIN_PATH = '~/AppData/Roaming/Mozilla/Firefox/Profiles/';
+const LINUX_PATH = '~/.mozilla/firefox/';
 
 
 class FirefoxCommand extends Command {
 	static label = 'firefoxBookmarks';
+	static path = '';
 
 	constructor(data) {
 		super();
@@ -16,21 +21,32 @@ class FirefoxCommand extends Command {
 		this.title = data.title;
 		this.keyword = data.title;
 		this.url = data.uri;
-		this.icon = data.iconuri || 'firefox';
+		this.icon = data.iconuri || data.iconUri || 'firefox';
+		this.profile = data.profile;
 	}
 
 	static parseDefinitions(data) {
-		const dir = data.replace('~', homedir);
+		FirefoxCommand.path = data.path;
+
+		const path = data.profileFolder || (win => win? WIN_PATH : LINUX_PATH)(process.platform === 'win32');
+		const dir = path.replace('~', homedir);
+
+		if (!fs.existsSync(dir)) {
+			return [];
+		}
 
 		return fs.readdirSync(dir)
-			.sort()
-			.slice(-1)
-			.flatMap(file => {
-				const fileBuffer = fs.readFileSync(dir+file);
-				const decompressedJson = jsonlz4(fileBuffer);
-				return recollect(decompressedJson.children);
-			})
-			.filter(bm => bm.title);
+			.filter(profile => filterExcludes(profile, data.exclude))
+			.filter(profile => fs.existsSync(`${dir}/${profile}/bookmarkbackups/`))
+			.flatMap(profile =>
+				fs.readdirSync(`${dir}/${profile}/bookmarkbackups/`)
+					.sort()
+					.slice(-1)
+					.map(file => fs.readFileSync(`${dir}/${profile}/bookmarkbackups/${file}`))
+					.flatMap(fileBuffer => recollect(jsonlz4(fileBuffer).children))
+					.map(bookmark => ({ ...bookmark, profile: getProfileName(profile)}))
+			)
+			.filter(bookmark => bookmark.title);
 	}
 
 	match(inputText) {
@@ -42,7 +58,11 @@ class FirefoxCommand extends Command {
 	}
 
 	perform() {
-		shell.openExternal(this.url);
+		if (FirefoxCommand.path) {
+			spawn(FirefoxCommand.path, ['-p', this.profile, this.url], { detached: true });
+		} else {
+			shell.openExternal(this.url);
+		}
 	}
 }
 
@@ -51,6 +71,18 @@ function recollect(json) {
 		...json.filter(j => j.type === 'text/x-moz-place'),
 		...json.filter(j => j.children).flatMap(j => recollect(j.children))
 	];
+}
+
+function filterExcludes(profile, excludes) {
+	if (excludes) {
+		return excludes.indexOf(getProfileName(profile)) < 0;
+	}
+	return true;
+}
+
+function getProfileName(profile) {
+	const [code, ...name] = profile.split('.');
+	return name.join('.');
 }
 
 module.exports = FirefoxCommand;
